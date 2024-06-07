c0aa5e2759a2ee1f4aa890ced57c441fd5446a95
arylka <yukinowak8@gmail.com>
2024-06-07 10:33:24 +0200

    Add notes on functions

---
+++ functions/what-are-functions.py
@@ -0,0 +1,27 @@
+# Functions are interactions of objects.
+## A function can take some objects and always gives something in return
+## (even with no return statement, it's the None object).
+
+# What function takes as its parameters is called a signature.
+def func(a, /, b, *, c):
+    #    ^^^^^^^^^^^^^
+    # What is that?
+    # - parameters
+    # - arguments
+    pass
+
+import inspect  # noqa
+print(inspect.signature(func))  # The signature is (a, /, b, *, c).
+
+# Every function can take parameters and always returns something:
+def func():
+    return 1  # The return value of func() is always 1.
+
+print(func())  # 1
+
+def func():
+    pass
+
+#   ^^^^ 
+# No return statement; the return value is always None.
+
